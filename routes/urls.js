f1653f9eef2f9deeeeec5e027e41105834f1df89
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-02-05 13:52:18 +0300

    Urls of API

--- routes/urls.js
+++ routes/urls.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const { registerUser, loginUser, update_User, delete_User, find_user } = require('../controller/logics');
 
+router.get('/', (req, res) => {
+    res.send(` Welcome to User Authentication API which is built using Nodejs, Expressjs, and PostgreSQL
+        This API allows you to register, login, update, delete, and find users in the PostgreSQL database.
+        Deveoloper: Mlue Technologies`);
+});
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/update/:id', update_User);
